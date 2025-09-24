358f0f2388426d8f3a08dd1f91fab49fb337c339
j-doniwa <juri-doniwa@kiseki.systems>
2025-09-24 09:53:26 +0900

    add: Case03.java 初回ログイン済みのユーザーでログインするテストを追加
    タイトルを取得し、期待値通りかを確認するテストを作成した
    期待値
    ・タイトル：コース詳細 | LMS

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,7 +1,11 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +13,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -34,15 +42,38 @@ public class Case03 {
 	@Test
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
-	void test01() {
-		// TODO ここに追加
+	void test01() throws Exception {
+		webDriver.get("http://localhost:8080/lms");
+		final String title = webDriver.getTitle();
+		assertEquals("ログイン | LMS", title);
+
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("evidence\\case3_1.png"));
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
+	void test02() throws Exception {
+		final WebElement loginId = webDriver.findElement(By.name("loginId"));
+		final WebElement password = webDriver.findElement(By.name("password"));
+		final WebElement loginButton = webDriver.findElement(By.cssSelector(".btn.btn-primary"));
+
+		loginId.clear();
+		loginId.sendKeys("StudentAA02");
+
+		password.clear();
+		password.sendKeys("StudentAA02");
+
+		loginButton.click();
+
+		Thread.sleep(5000);
+
+		final String title = webDriver.getTitle();
+		assertEquals("コース詳細 | LMS", title);
+
+		File file = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("evidence\\case3_2.png"));
 	}
 
 }
